676ec000f875ce3f84dfcaa32d7a83bf7c8ed65d
Adam Shepard <adam.w.gangl@gmail.com>
2023-04-08 16:12:07 -0500

    can successfully add a movie title, poster and description.

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -21,12 +21,12 @@ router.get('/:id', (req, res) => {
   const selectedId = req.params.id
   const queryText =
     `SELECT "movies".id, "movies".title, "movies".poster, "movies".description, 
-array_to_string(array_agg("genres".name), ', ') AS genres
-FROM "movies"
-LEFT JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
-LEFT JOIN "genres" ON "movies_genres".genre_id = "genres".id
-WHERE "movies".id = $1
-GROUP BY "movies".id, "movies".title, "movies".poster, "movies".description`;
+      array_to_string(array_agg("genres".name), ', ') AS genres
+      FROM "movies"
+      LEFT JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
+      LEFT JOIN "genres" ON "movies_genres".genre_id = "genres".id
+      WHERE "movies".id = $1
+      GROUP BY "movies".id, "movies".title, "movies".poster, "movies".description`;
   pool.query(queryText, [selectedId])
     .then(result => {
       res.send(result.rows);
@@ -42,9 +42,9 @@ router.post('/', (req, res) => {
   console.log(req.body);
   // RETURNING "id" will give us back the id of the created movie
   const insertMovieQuery = `
-  INSERT INTO "movies" ("title", "poster", "description")
-  VALUES ($1, $2, $3)
-  RETURNING "id";`
+    INSERT INTO "movies" ("title", "poster", "description")
+    VALUES ($1, $2, $3)
+    RETURNING "id";`
 
   // FIRST QUERY MAKES MOVIE
   pool.query(insertMovieQuery, [req.body.title, req.body.poster, req.body.description])
@@ -55,9 +55,8 @@ router.post('/', (req, res) => {
 
       // Now handle the genre reference
       const insertMovieGenreQuery = `
-      INSERT INTO "movies_genres" ("movie_id", "genre_id")
-      VALUES  ($1, $2);
-      `
+        INSERT INTO "movies_genres" ("movie_id", "genre_id")
+        VALUES  ($1, $2);`
       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
       pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
         //Now that both are done, send back success!
--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,76 +1,76 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function AddMovieForm() {
 
     const dispatch = useDispatch();
     const genres = useSelector(store => store.genres);
 
-    let [movieToAdd, setMovieToAdd] = useState({ title: '', poster: '', description: '' })
-    let [genreToAdd, setGenreToAdd] = useState({ id: '', name: '' })
+    let [movieToAdd, setMovieToAdd] = useState({ title: '', poster: '', description: '', genre_id: '' })
 
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MOVIES' });
+        dispatch({ type: 'FETCH_GENRES' })
+    }, []);
 
-    const handleTitleChange = (event) => {
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
         setMovieToAdd({
             ...movieToAdd,
-            title: event.target.value,
-        });
+            [name]: value,
+        })
     }
 
-    const handlePosterChange = (event) => {
-        setMovieToAdd({
-            ...movieToAdd,
-            poster: event.target.value,
-        });
-    }
+    const addMovie = (movieToAdd) => {
+        console.log('Inside of addMovie function. Here is movieToAdd:', movieToAdd)
+        if (movieToAdd.title === ''|| movieToAdd.poster === '' || movieToAdd.description === '') {
+            alert('You must complete all input fields!')
+        }
+        else {
+            dispatch({
+                type: 'ADD_NEW_MOVIE',
+                payload: movieToAdd
+            })
+        }
 
-    const handleDescriptionChange = (event) => {
-        setMovieToAdd({
-            ...movieToAdd,
-            description: event.target.value,
-        });
     }
-
-    const handleGenreChange = (event) => {
-        setGenreToAdd({
-            ...genreToAdd,
-            name: event.target.value,
-        });
-    }
-
-    
 
     return (
         <main>
-            <form onSubmit={(event) => addMovie(event)}>
+            <form onSubmit={(event) => addMovie(movieToAdd)}>
                 <input
-                    onChange={handleTitleChange}
+                    onChange={handleInputChange}
                     type='text'
                     placeholder='title'
+                    name='title'
                     value={movieToAdd.title}
                 />
                 <input
-                    onChange={handlePosterChange}
+                    onChange={handleInputChange}
                     type='text'
                     placeholder='poster'
+                    name='poster'
                     value={movieToAdd.poster}
                 />
                 <input
-                    onChange={handleDescriptionChange}
+                    onChange={handleInputChange}
                     type='text'
                     placeholder='description'
+                    name='description'
                     value={movieToAdd.description}
                 />
-                <select id="genres" name="genres"
-                    onChange={handleGenreChange}
-                    value={genreToAdd.name}>
+                <select
+                    id="genres"
+                    name="genres_id"
+                    onChange={handleInputChange}
+                    value={movieToAdd.genre_id}>
                     {genres.map((genre) => (
-                        <option key={genre.id} value={genre.name}>
+                        <option key={genre.id} value={genre.id}>
                             {genre.name}
                         </option>
                     ))}
                 </select>
-
 
                 <button type='submit'>Submit</button>
             </form>
