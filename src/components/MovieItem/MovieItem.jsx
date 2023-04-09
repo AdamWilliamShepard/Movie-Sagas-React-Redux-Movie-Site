45832b16adaf3bf302bb9370582811bbf88a13da
Adam Shepard <adam.w.gangl@gmail.com>
2023-04-08 22:27:44 -0500

    added background color and hover effect to cards on Movie List page

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -25,24 +25,34 @@ function MovieItem({ movie }) {
     }
 
     return (<>
-        <Card sx={{ maxWidth: 500, backgroundColor: 'rgba(32, 119, 212, 0.3)'}}>
-                <CardMedia
-                    sx={{ height: 325, width: "auto", justifyContent: "center"}}
-                    image={movie.poster}
-                    title={movie.title}
-                    onClick={showDescription}
-                />
-                <CardContent sx={{ width: '100%' }}>
-                    <form onSubmit=''>
-                        <Typography gutterBottom variant="h5" component="div">
+        <Card sx={{
+            maxWidth: 500,
+            backgroundColor: 'rgba(32, 119, 212, 0.3)',
+            cursor: 'grab',
+            transition: 'all 0.2s ease-in-out',
+            '&:hover': {
+                backgroundColor: 'rgba(32, 119, 212, 0.6)',
+                boxShadow: '0px 0px 20px 5px rgba(0,0,0,0.5)',
+                transform: 'scale(1.05)'
+            }
+        }}>
+            <CardMedia
+                sx={{ height: 325, width: "auto", justifyContent: "center" }}
+                image={movie.poster}
+                title={movie.title}
+                onClick={showDescription}
+            />
+            <CardContent sx={{ width: '100%' }}>
+                <form onSubmit=''>
+                    <Typography gutterBottom variant="h5" component="div">
                         {movie.title}
-                        </Typography>
-                    </form>
-                </CardContent>
-                <CardActions sx={{ justifyContent: "center" }}>
+                    </Typography>
+                </form>
+            </CardContent>
+            <CardActions sx={{ justifyContent: "center" }}>
 
-                </CardActions>
-            </Card>
+            </CardActions>
+        </Card>
 
     </>
     );
